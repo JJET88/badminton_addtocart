4aedc45a8753ac890e615784281ac6e77495aa5c
Ko-aungKo <yyaa82162@gmail.com>
2025-12-06 23:06:39 +0700

    test5

--- proxy.js
+++ proxy.js
@@ -1,32 +1,33 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
+export default function proxy(req) {
+  const token = req.cookies.get("token")?.value;
 
-export function middleware(req) {
-const token = req.cookies.get("token")?.value;
+  // No token
+  if (!token) {
+    if (
+      req.nextUrl.pathname.startsWith("/admin") ||
+      req.nextUrl.pathname.startsWith("/user")
+    ) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+    return NextResponse.next();
+  }
 
+  // decode role
+  const decoded = jwt.decode(token);
 
-if (!token) {
-if (req.nextUrl.pathname.startsWith("/admin") || req.nextUrl.pathname.startsWith("/user")) {
-return NextResponse.redirect(new URL("/login", req.url));
+  if (
+    req.nextUrl.pathname.startsWith("/admin") &&
+    decoded.role !== "admin"
+  ) {
+    return NextResponse.redirect(new URL("/user", req.url));
+  }
+
+  return NextResponse.next();
 }
-return NextResponse.next();
-}
-
-
-// With token → check role
-const decoded = jwt.decode(token);
-
-
-if (req.nextUrl.pathname.startsWith("/admin") && decoded.role !== "admin") {
-return NextResponse.redirect(new URL("/user", req.url));
-}
-
-
-return NextResponse.next();
-}
-
 
 export const config = {
-matcher: ["/admin/:path*", "/user/:path*"],
+  matcher: ["/admin/:path*", "/user/:path*"],
 };
